80c5ea789fa6b2b37f0e439b9c5dfd330dd21acd
Elias Gonzalez <eliasivang@gmail.com>
2024-03-04 00:53:41 -0300

    listofvessels

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -10,34 +10,27 @@ import { Vessel} from './user.interface';
 
 
 export class FormularioComponent {
-
-  
-  
-  
     public practiceForm = new FormGroup({
-      name: new FormControl(" ", [Validators.required, Validators.minLength(2)]),
-      vessel: new FormControl(" ", Validators.required),
-      numeroIMO: new FormControl(" ", [Validators.required, Validators.email]),
-      adress: new FormControl(" ", Validators.required),
-      adress2: new FormControl(""),
-      city: new FormControl(" ", Validators.required),
-      email: new FormControl(" ", Validators.required),
+      name: new FormControl("", [Validators.required, Validators.minLength(2)]),
+      bandera: new FormControl("", Validators.required),
+      numeroimo: new FormControl<number>(0, Validators.required),
+      vesseltype: new FormControl( "", Validators.required),
+
     });
   
-
-
     public vessels: any[] = [];
-    
 
     resetForm(): void {
       this.practiceForm.reset()
     }
     
-    addVessel(){
-      this.vessels.push(this.practiceForm.value)
-      console.log(this.vessels)
-      this.resetForm()
-    }
+    addVessel():void {
+        const nuevoRegistro = this.practiceForm.value;
+        this.vessels.push(nuevoRegistro);
+        // Puedes reiniciar el formulario si es necesario
+        this.practiceForm.reset();
+    
+  }
 
 
 
